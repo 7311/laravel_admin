649e51b24b88da3ea1ab5ec1dc162cfeeddd9df6
Steven <wsxrtgvbuikm@gmail.com>
2023-02-11 22:59:31 +0800

    增加users

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -12,23 +12,37 @@ class HomeController extends Controller
 {
     public function index(Content $content)
     {
-        return $content
-            ->title('Dashboard')
+        $content
+            ->title('主控台')
             ->description('Description...')
-            ->row(Dashboard::title())
-            ->row(function (Row $row) {
+            ->breadcrumb(
+                ['text' => '首页', 'url' => '/admin'],
+                ['text' => '用户管理', 'url' => '/admin/users'],
+                ['text' => '编辑用户'])
+            ->body('hello world')
+            ->body('foo bar')
+            ->row('hello world')
+        ;
 
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::environment());
-                });
+        return $content;
+        
+        // return $content
+        //     ->title('Dashboard')
+        //     ->description('Description...')
+        //     ->row(Dashboard::title())
+        //     ->row(function (Row $row) {
 
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::extensions());
-                });
+        //         $row->column(4, function (Column $column) {
+        //             $column->append(Dashboard::environment());
+        //         });
 
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::dependencies());
-                });
-            });
+        //         $row->column(4, function (Column $column) {
+        //             $column->append(Dashboard::extensions());
+        //         });
+
+        //         $row->column(4, function (Column $column) {
+        //             $column->append(Dashboard::dependencies());
+        //         });
+        //     });
     }
 }
