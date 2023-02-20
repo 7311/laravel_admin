52802a503bc8350519f3000aedee0c3c9b42ed63
Steven <wsxrtgvbuikm@gmail.com>
2023-02-20 23:31:14 +0800

    註冊、登入改成username登入

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,6 +10,11 @@
             @csrf
 
             <div>
+                <x-label for="username" value="{{ __('User Name') }}" />
+                <x-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="name" />
+            </div>
+
+            <div>
                 <x-label for="name" value="{{ __('Name') }}" />
                 <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
